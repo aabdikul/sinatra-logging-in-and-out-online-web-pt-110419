6b43af7fc591bfd5d02e5acd4d043f2fa874c6c3
Aida Abdikulova <abdikulova@gmail.com>
2020-03-01 23:42:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
+    binding.pry
     if @user
       @user.id = session[:user_id]
       redirect "/account"
