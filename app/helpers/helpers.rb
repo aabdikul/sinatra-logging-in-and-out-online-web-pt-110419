2831741d6d0abf5c16a502c37a07501b31493b68
Aida Abdikulova <abdikulova@gmail.com>
2020-03-01 23:33:17 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,8 @@
 class Helpers
 
   def self.current_user(session)
-    @user = User.find_by(id: session[:user_id])
-    @user
+    user = User.find_by(id: session[:user_id])
+    user
   end
 
   def self.is_logged_in?(session)
